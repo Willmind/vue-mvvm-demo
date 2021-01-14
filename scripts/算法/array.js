4cf1141a4f4b349d26a3e036c894e02cdbe2e055
Willmind <tanzhijingege@126.com>
2021-01-14 17:08:55 +0800

    更新数组算法题

--- scripts/算法/array.js
+++ scripts/算法/array.js
@@ -1,4 +1,3 @@
-
 /** 删除排序数组中的重复项
  * @param {number[]} nums
  * @return {number}
@@ -65,7 +64,143 @@ var containsDuplicate = function (nums) {
  * @param {number[]} digits
  * @return {number[]}
  */
-var plusOne = function(digits) {
+var plusOne = function (digits) {
+    let last = digits.length - 1
+    while (last >= 0) {
+        if (digits[last] < 9) {
 
+            digits[last] += 1;
+            return digits
+        } else {
+            digits[last] = 0
+        }
+        last--
+
+    }
+    //首位字符为0的情况
+    if (digits[0] === 0) {
+        digits.unshift(1)
+
+    }
+    return digits
 
 };
+
+/**移动零
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let start = 0;
+    let end = nums.length - 1
+    while (start < end) {
+        if (nums[start] === 0) {
+            nums.splice(start, 1)
+            nums.push(0)
+            end--
+            continue
+        }
+        start++;
+    }
+    return nums
+
+};
+
+
+/** 有效的数独
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function (board) {
+
+    //判断每一行
+    for (let arr of board) {
+        let row = [];
+        for (let cell of arr) {
+            if (cell !== '.') {
+                row.push(cell)
+            }
+        }
+        let set = new Set(row) //es6数组去重（值唯一）
+        if (set.size !== row.length) {
+            return false;
+
+        }
+    }
+
+    //判断每一列
+    for (let i = 0; i < 9; i++) {
+        let col = [];
+        board.map(arr => {
+            if (arr[i] !== '.') {
+                col.push(arr[i])
+            }
+        })
+        let set = new Set(col)
+        if (set.size !== col.length) {
+            return false;
+
+        }
+
+    }
+
+    //判断每一个九宫格
+    for (let x = 0; x < 9; x += 3) { //x轴
+        for (let y = 0; y < 9; y += 3) {  //y轴
+
+            //循环每个小的9宫格
+            let box = [];
+            for (let i = x; i < x + 3; i++) {
+                for (let j = y; j < y + 3; j++) {
+                    if (board[i][j] !== '.') {
+                        box.push(board[i][j])
+                    }
+
+                }
+            }
+            let set = new Set(box)
+            if (set.size !== box.length) {
+                return false;
+
+            }
+
+
+        }
+    }
+
+    return true;
+
+};
+
+/** 旋转图像
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+var rotate = function (matrix) {
+
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            let tmp = matrix[i].shift();
+            matrix[j].push(tmp);
+        }
+    }
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i] = matrix[i].reverse()
+
+    }
+    return matrix
+}
+
+/**
+ * 整数反转
+
+ * @param num
+ * @returns {*}
+ */
+function reverse(num) {
+    var reverseNum = (Math.abs(num) + '').split('').reverse().join('');
+    //判断是否超出范围
+    return (reverseNum > Math.pow(2, 31) - 1 || reverseNum < Math.pow(2, 31)*-1) ?
+        0 :
+        num > 0 ? reverseNum : -reverseNum;
+}
